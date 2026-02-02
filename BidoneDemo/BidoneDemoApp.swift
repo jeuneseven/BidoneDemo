143ef97b89240fd185724123d4bf897239a904a5
jeuneseven <jeuneseven@gmail.com>
2026-02-02 17:38:29 +1300

    Initial Commit

---
+++ BidoneDemo/BidoneDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BidoneDemoApp.swift
+//  BidoneDemo
+//
+//  Created by seven on 2026/2/2.
+//
+
+import SwiftUI
+
+@main
+struct BidoneDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BidoneDemoTests/BidoneDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  BidoneDemoTests.swift
+//  BidoneDemoTests
+//
+//  Created by seven on 2026/2/2.
+//
+
+import Testing
+@testable import BidoneDemo
+
+struct BidoneDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
